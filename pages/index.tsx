6b33b2c12dfda9f3406e8ef626031018caad7549
Bob <bkane90@gmail.com>
2023-06-09 16:23:02 -0400

    add background images, add and style main container, text, and button for generator, add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { GradientBackground } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+//Components
+import {
+	BackgroundImage1,
+	BackgroundImage2,
+	GradientBackground,
+	Footer,
+	CustomLink,
+	QuoteGeneratorCon,
+	QuoteGeneratorInnerCon,
+	QuoteGeneratorTitle,
+	QuoteGeneratorSubTitle,
+	GenerateQuoteButton,
+	GenerateQuoteButtonText,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+//Assets
+import LightningCloud from "../assets/lightning-cloud.png";
+import SunCloud from "../assets/cloud-and-sun.png";
 
 export default function Home() {
+	const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
 	return (
 		<>
 			<Head>
@@ -16,7 +36,41 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			{/* Background */}
-			<GradientBackground></GradientBackground>
+			<GradientBackground>
+				{/* Quote Generator Modal Pop-Up*/}
+				{/* <QuoteGeneratorModal /> */}
+
+				{/* Quote Generator */}
+				<QuoteGeneratorCon>
+					<QuoteGeneratorInnerCon>
+						<QuoteGeneratorTitle>Daily Inspiration Quote Generator</QuoteGeneratorTitle>
+
+						<QuoteGeneratorSubTitle>
+							In need of some inspiration? Generate a quote card with a random inspirational quote provided
+							by{" "}
+							<CustomLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">
+								ZenQuotes API
+							</CustomLink>
+							.
+						</QuoteGeneratorSubTitle>
+						<GenerateQuoteButton>
+							<GenerateQuoteButtonText>Generate Quote</GenerateQuoteButtonText>
+						</GenerateQuoteButton>
+					</QuoteGeneratorInnerCon>
+				</QuoteGeneratorCon>
+
+				{/* Background Images */}
+				<BackgroundImage1 src={LightningCloud} height="300" alt="Cloud with lightning bolt" />
+				<BackgroundImage2 src={SunCloud} height="300" alt="Clouds" />
+				{/* Footer */}
+				<Footer>
+					<>
+						Quotes Generated: {numberOfQuotes}
+						<br />
+						Developed by Bob Kane
+					</>
+				</Footer>
+			</GradientBackground>
 		</>
 	);
 }
