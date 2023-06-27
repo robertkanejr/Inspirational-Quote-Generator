8e4388498e2a844618fc820a3db80f530533e25b
Bob <bkane90@gmail.com>
2023-06-27 17:32:33 -0400

    add local mock of quote hover for image blob

--- components/animations/ImageBlob.tsx
+++ components/animations/ImageBlob.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 interface ImageBlobProps {
@@ -7,7 +7,31 @@ interface ImageBlobProps {
 }
 
 const ImageBlob = ({ quoteReceived, blobUrl }: ImageBlobProps) => {
-	return <div>ImageBlob</div>;
+	// const [blobUrl, setBlobUrl] = useState<string | null>(null);
+
+	// // Simulate fetching response object from Lambda
+	// useEffect(() => {
+	// 	const response = {
+	// 		statusCode: 200,
+	// 		headers: {
+	// 			"Content-Type": "image/png",
+	// 			"Access-Control-Allow-Origin": "*",
+	// 		},
+	// 			"body": blobURL,
+	// 		isBase64Encoded: true,
+	// 	};
+	// 	const binaryData = Buffer.from(response.body, "base64");
+	// 	const blob = new Blob([binaryData], { type: response.headers["Content-Type"] });
+	// 	const newBlobUrl = URL.createObjectURL(blob);
+	// 	setBlobUrl(newBlobUrl);
+	// 	return () => {
+	// 		URL.revokeObjectURL(newBlobUrl);
+	// 	};
+	// }, []);
+
+	if (!blobUrl) return null;
+
+	return <Image src={blobUrl} alt="Generated quote card" width={150} height={100} />;
 };
 
 export default ImageBlob;
